82c1481003fe30e8fd13e950fc0d74f67fc18770
urevoleg <urevolegg@gmail.com>
2022-12-21 16:26:49 +0300

    add projection

--- src/dags/sql/stg/transactions.sql
+++ src/dags/sql/stg/transactions.sql
@@ -13,3 +13,10 @@ order by transaction_dt
 SEGMENTED BY hash(operation_id, transaction_dt::date) all nodes
 PARTITION BY transaction_dt::date
 GROUP BY calendar_hierarchy_day(transaction_dt::date, 3, 2);
+
+
+CREATE PROJECTION IF NOT EXISTS transactions_by_dates as
+SELECT *
+FROM UREVOLEGYANDEXRU__STAGING.transactions
+ORDER BY transaction_dt
+SEGMENTED BY hash(transaction_dt::date) all nodes;
